a41e002a4a8ea6bdd69358b537e4adda5ba88ff1
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-28 18:10:14 +0200

    Added spinner animation as a function before recipeContainer is loaded

--- src/js/controller.js
+++ src/js/controller.js
@@ -17,10 +17,22 @@ console.log('TEST');
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+  <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
     // 1) Loading recipe
-
+    renderSpinner(recipeContainer);
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8f7'
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
