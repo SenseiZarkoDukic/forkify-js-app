bc8ccecb675ca0a9de2c4a1e5f9c7441f7fb30b5
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-29 19:41:59 +0200

    Created loadRecipe async function with code and function part moved from controller.js file created for getting data for the recipes.

--- src/js/controller.js
+++ src/js/controller.js
@@ -41,28 +41,6 @@ const showRecipe = async function () {
     if (!id) return;
     // 1) Loading recipe
     renderSpinner(recipeContainer);
-    const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8f7'
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res, data);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceURl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
 
     // 2) Rendering recipe
     const markup = `
