9fe12dde86cbb7a6eb54304c43afac2ad1484bd0
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-09-03 17:47:18 +0200

    Finnished handling errors

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,9 +1,17 @@
+import { async } from 'regenerator-runtime';
+import { TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
 export const getJSON = async function (url) {
   try {
-    const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8f7'
-      url
-    );
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
